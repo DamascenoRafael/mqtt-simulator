a0e59501fdf282177c03f431546e8cc868aeefac
Rafael Damasceno <damscn@gmail.com>
2025-05-24 22:30:12 -0300

    Minor typing improvement

--- mqtt-simulator/main.py
+++ mqtt-simulator/main.py
@@ -2,12 +2,12 @@ import argparse
 from pathlib import Path
 from simulator import Simulator
 
-def default_settings():
+def default_settings() -> Path:
     base_folder = Path(__file__).resolve().parent.parent
     settings_file = base_folder / "config/settings.json"
     return settings_file
 
-def is_valid_file(arg):
+def is_valid_file(arg: str) -> Path:
     settings_file = Path(arg)
     if not settings_file.is_file():
         raise argparse.ArgumentTypeError(f"argument -f/--file: can't open '{arg}'")
